60fb6c7509df46b57640064e49bfe7ed3ccbb09b
Alex Creasy <acreasy@redhat.com>
2023-05-02 14:58:26 +0100

    Force enable model metrics for summit demo

--- frontend/src/pages/modelServing/useModelMetricsEnabled.ts
+++ frontend/src/pages/modelServing/useModelMetricsEnabled.ts
@@ -1,14 +1,16 @@
-import { useAppContext } from '~/app/AppContext';
-import { useDashboardNamespace } from '~/redux/selectors';
-import { isModelMetricsEnabled } from './screens/metrics/utils';
+// import { useAppContext } from '~/app/AppContext';
+// import { useDashboardNamespace } from '~/redux/selectors';
+// import { isModelMetricsEnabled } from './screens/metrics/utils';
 
-const useModelMetricsEnabled = (): [modelMetricsEnabled: boolean] => {
-  const { dashboardNamespace } = useDashboardNamespace();
-  const { dashboardConfig } = useAppContext();
+// const useModelMetricsEnabled = (): [modelMetricsEnabled: boolean] => {
+//   const { dashboardNamespace } = useDashboardNamespace();
+//   const { dashboardConfig } = useAppContext();
+//
+//   const checkModelMetricsEnabled = () => isModelMetricsEnabled(dashboardNamespace, dashboardConfig);
+//
+//   return [checkModelMetricsEnabled()];
+// };
 
-  const checkModelMetricsEnabled = () => isModelMetricsEnabled(dashboardNamespace, dashboardConfig);
-
-  return [checkModelMetricsEnabled()];
-};
+const useModelMetricsEnabled = () => [true];
 
 export default useModelMetricsEnabled;
