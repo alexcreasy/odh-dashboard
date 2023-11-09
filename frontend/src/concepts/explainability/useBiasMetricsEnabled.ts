e197d87d5262e150fe2f78a94ee0bd977e1b03c9
Alex Creasy <alex@creasy.dev>
2023-11-09 13:13:07 +0000

    First pass at improving BiasMetrics detection

--- frontend/src/concepts/explainability/useBiasMetricsEnabled.ts
+++ frontend/src/concepts/explainability/useBiasMetricsEnabled.ts
@@ -1,5 +1,11 @@
 import { SupportedArea, useIsAreaAvailable } from '~/concepts/areas';
 
-const useBiasMetricsEnabled = () => [useIsAreaAvailable(SupportedArea.TRUSTY_AI).status];
+const useBiasMetricsEnabled = () => {
+  const { status, featureFlags } = useIsAreaAvailable(SupportedArea.TRUSTY_AI);
+
+  const enabled = status && featureFlags?.disableBiasMetrics === 'on';
+
+  return [enabled];
+};
 
 export default useBiasMetricsEnabled;
