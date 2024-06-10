af64a142966824a6cabd7f661456725d85246892
Alex Creasy <alex@creasy.dev>
2024-06-10 11:32:14 +0100

    WIP

--- frontend/src/concepts/metrics/kserve/content/KserveMemoryUsageGraph.tsx
+++ frontend/src/concepts/metrics/kserve/content/KserveMemoryUsageGraph.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { KserveMetricGraphDefinition } from '~/concepts/metrics/kserve/types';
 import { TimeframeTitle } from '~/concepts/metrics/types';
-// TODO: remove this, figure out where to put hook!!!!!!
+// TODO: remove this, figure out where to put the hook!!!!!!
 // eslint-disable-next-line no-restricted-imports
 import { useFetchKserveMemoryUsageData } from '~/api/prometheus/kservePerformanceMetrics';
 import MetricsChart from '~/pages/modelServing/screens/metrics/MetricsChart';
